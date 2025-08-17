f33de64cfdb3ba27ef6f4914ab19bc9496795e43
Minttu Gomez <minttu.jamsa@helsinki.fi>
2025-08-17 22:08:16 +0300

    add sql-table for recipes

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,3 +3,12 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE recipes (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    title TEXT NOT NULL,
+    description TEXT,
+    ingredients TEXT NOT NULL,
+    instructions TEXT NOT NULL
+);
